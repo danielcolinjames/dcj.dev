1189b71d4092f65640b403996605f9ccb5a29fda
Daniel James <danielcolinjames@gmail.com>
2019-02-05 17:21:28 -0800

    Add highlights to homepage

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { Fragment } from 'react';
+import MetaTags from 'react-meta-tags';
+
 import Header from '../Components/Header';
 import IntroSection from '../Components/IntroSection';
 import ProjectsSection from '../Components/ProjectsSection';
+import HighlightsSection from '../Components/HighlightsSection';
 
 export default (props) => {
     return (
         <Fragment>
+            <MetaTags>
+                <title>Daniel Colin James</title>
+                <meta name="theme-color" content="#EFEFEF"/>
+            </MetaTags>
             <Header background="#EFEFEF"/>
             <IntroSection />
             <ProjectsSection />
+            <HighlightsSection />
         </Fragment>
     )
 }
