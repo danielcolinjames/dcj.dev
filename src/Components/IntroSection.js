9dc68142d84912f6a905abb851000e01e475b6e5
Daniel James <danielcolinjames@gmail.com>
2019-02-05 17:17:29 -0800

    Change component hierarchy (don't have separate file for every component)

--- src/Components/IntroSection.js
+++ src/Components/IntroSection.js
@@ -1,17 +1,17 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import React from 'react';
 
 import picture from '../images/testoutput.png';
 
 
-const IntroBackgroundContainer = styled.div`
+export const IntroBackgroundContainer = styled.div`
     background-color: ${props => props.bgc};
-    min-height: 500px;
+    min-height: 400px;
     width: 100%;
 
     display: grid;
-    grid-template-columns: 1fr 1.5fr;
-    grid-template-rows: 1fr 1fr;
+    grid-template-columns: auto;
+    grid-template-rows: auto;
 
     grid-template-areas:
         "picture header"
@@ -21,40 +21,59 @@ const IntroBackgroundContainer = styled.div`
         min-height: 200px;
 
         grid-template-columns: 2fr 1fr;
-        grid-template-rows: auto 1fr;
+        grid-template-rows: auto auto auto;
 
         grid-template-areas:
             "header header"
-            "body picture";
+            "body body"
+            "picture picture";
+            /* "body picture"; */
             /* "picture picture"; */
     }
+    /* Change things when this component has the "project" prop attached */
+    ${props => props.project && css`
+        
+    `}
 `;
 
-const IntroTextH1 = styled.h1`
+export const IntroTextH1 = styled.h1`
+    color: black;
     grid-area: header;
     font-size: 3em;
     font-family: "Europa-Bold";
         
     max-width: 600px;
     text-align: left;
-    margin: 0 30px 0 30px;
+    margin: 50px 30px 0 30px;
     
     /* width: 100%; */
 
     align-self: end;
     justify-self: start;
 
-    /* background-color: blue; */
+    @media (max-width: 700px) {
+        /* font-size: 2em; */
+        margin: 50px 50px 0 50px;
+    }
+
+    ${props => props.project && css`
+        font-size: 5em;
+        color: #4B4B4B;
+
+        @media(max-width: 1000px) {
+            font-size: 4em;
+        }
+    `}
 `
 
-const IntroTextH2 = styled.p`
+export const IntroTextH2 = styled.p`
     grid-area: body;
     font-size: 1.5em;
     font-family: "Europa-Light";
     
-    max-width: 600px;
+    max-width: 550px;
     text-align: left;
-    margin: 10px 30px 0 30px;
+    margin: 10px 30px 50px 30px;
 
     /* width: 100%; */
 
@@ -64,37 +83,73 @@ const IntroTextH2 = styled.p`
     /* background-color: red; */
 
     @media (max-width: 700px) {
-        font-size: 1.25em;
-        margin-right: 20px;
-        margin-bottom: 50px;
+        /* font-size: 1.25em; */
+        margin: 10px 40px 10px 50px;
+        min-width: 200px;
     }
+
+    ${props => props.project && css`
+        font-family: "Europa-Bold";
+        font-size: 2em;
+        color: #666;
+
+        @media (max-width: 700px) {
+            /* font-size: 1.25em; */
+            margin: 10px 40px 10px 50px;
+            min-width: 200px;
+        }
+    `}
 `
 
-const IntroPicture = styled.img.attrs({
-    src: picture
+export const IntroPicture = styled.img.attrs({
+    src: props => props.imgsrc
 })`
     grid-area: picture;
 
-    max-width: 350px;
-    width: 100%;
-    margin-top: auto;
-    margin-right: 20px;
-    margin-left: 20px;
+    max-width: 250px;
+    max-height: 370px;
+    width: auto;
+
+    /* margin-top: ${props => props.margintop}; */
+
+    /* margin-right: 0px; */
+    margin-left: 50px;
 
     /* background-color: red; */
 
     justify-self: end;
+    align-self: ${props => props.alignment};
 
     @media (max-width: 700px) {
         /* max-width: 40%; */
+        max-width: 200px;
+        /* width: auto; */
         height: auto;
+        margin-top: 20px;
+        margin-right: 50px;
+        margin-bottom: ${props => props.marginBottom};
+        justify-self: ${props => props.justifyMobile};
+        align-self: ${props => props.alignmentMobile};
     }
+
+    ${props => props.project && css`
+        margin-right: 50px;
+        max-width: 250px;
+        height: auto;
+
+        @media(max-width: 1000px) {
+            /* font-size: 4em; */
+            margin-top: 30px;
+        }
+    `}
 `;
 
 export default () => {
     return (
         <IntroBackgroundContainer bgc="#EFEFEF">
-            <IntroPicture />
+            <IntroPicture
+                imgsrc={picture}
+                alignment='end' />
 
             <IntroTextH1>
                 Hey, I'm Daniel.
@@ -104,6 +159,5 @@ export default () => {
             </IntroTextH2>
 
         </IntroBackgroundContainer>
-
     )
 }
